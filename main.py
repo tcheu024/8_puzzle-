1d7e6747e99dc5b0b68c2baf8655984d07f2e761
Tim C <timc@ucr-secure-40-10-13-211-190.wnet.ucr.edu>
2026-02-10 09:48:37 -0800

    Added custom puzzle function

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-initial_state = (1,2,3
-                ,4,5,6
-                ,7,8,0)
-
-goal_state = (1,2,3
+def default_puzzle():
+    return (1,2,3
             ,4,5,6
             ,7,8,0)
 
-print(initial_state)
-print(goal_state)
+def custom_puzzle():
+    print("Create your own puzzle, using a 0 to represent the blank tile.")
+    nums = []
+    rows = ["first", "second", "third"]
+
+    for i in range(3):
+        row = input ("Enter the " + rows[i] + " row, with spaces between the numbers: ")
+        row_nums = row.split()
+
+        nums.append(int(row_nums[0]))
+        nums.append(int(row_nums[1]))
+        nums.append(int(row_nums[2]))
+    
+    
+
+    return tuple(nums)
+
+
+puzzle = custom_puzzle()
+print("Puzzle tuple:", puzzle)
+
