15a22b42ea4a4004e72ff4bac508613a2294eddf
Tim C <timc@ucr-secure-40-10-13-211-190.wnet.ucr.edu>
2026-02-10 13:56:58 -0800

    Allows user to use multiple dimensions for the custom puzzle

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
+import heapq
+#The goal state for a 3x3 puzzle
+goal_state = (1,2,3
+              ,4,5,6
+              ,7,8,0)
+
+#Hardcoded default puzzle for testing purposes
 def default_puzzle():
     return (1,2,3
             ,4,5,6
             ,7,8,0)
 
+# Function for the user to create their own unique puzzle, can be of any size
 def custom_puzzle():
     print("Create your own puzzle, using a 0 to represent the blank tile.")
-    nums = []
-    rows = ["first", "second", "third"]
+    dimensions = int(input("How many rows and columns does your puzzle have? "))
 
-    for i in range(3):
-        row = input ("Enter the " + rows[i] + " row, with spaces between the numbers: ")
+    nums = []
+    for i in range(dimensions):
+       
+        row = input ("Enter the numbers for " + str(i+1) + " row, with spaces between the numbers: ")
         row_nums = row.split()
 
-        nums.append(int(row_nums[0]))
-        nums.append(int(row_nums[1]))
-        nums.append(int(row_nums[2]))
-    
-    
+        for num in row_nums:
+            nums.append(int(num))
 
+    if sorted(nums) != list(range(dimensions*dimensions)):
+        print("There should be only 1 of each number, Retry.")
+        return 0
+    
     return tuple(nums)
+
+def general_search(initial_state):
+    nodes = []
+    heapq.heappush(nodes, (0, initial_state))  
+
+    explored = set()
+    
+    pass
 
 
 puzzle = custom_puzzle()
 print("Puzzle tuple:", puzzle)
+
+
+
 
