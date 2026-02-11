264d69967619c1c09ed88edde8785206c6004fa7
Tim C <timc@Tims-MacBook-Pro.local>
2026-02-10 21:33:53 -0800

    Created general search algo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import heapq
-#The goal state for a 3x3 puzzle
+
+#The goal state for 3x3 puzzle
 goal_state = (1,2,3
               ,4,5,6
               ,7,8,0)
@@ -10,38 +11,61 @@ def default_puzzle():
             ,4,5,6
             ,7,8,0)
 
-# Function for the user to create their own unique puzzle, can be of any size
+#Fuction to create unique puzzle
 def custom_puzzle():
-    print("Create your own puzzle, using a 0 to represent the blank tile.")
-    dimensions = int(input("How many rows and columns does your puzzle have? "))
+    print("Create puzzle, using a 0 to represent the blank tile.")
+    dimension = int(input("How many rows and columns does your puzzle have? "))
 
     nums = []
-    for i in range(dimensions):
-       
-        row = input ("Enter the numbers for " + str(i+1) + " row, with spaces between the numbers: ")
+    for i in range(dimension):
+        row = input("Enter the numbers for " + str(i+1) + " row, with spaces between the numbers: ")
         row_nums = row.split()
 
         for num in row_nums:
             nums.append(int(num))
 
-    if sorted(nums) != list(range(dimensions*dimensions)):
+    if sorted(nums) != list(range(dimension*dimension)):
         print("There should be only 1 of each number, Retry.")
         return 0
-    
+
     return tuple(nums)
 
-def general_search(initial_state):
+def general_search(initial_state, QUEUING_FUNCTION):
+    #create a initial node with the initial state
     nodes = []
     heapq.heappush(nodes, (0, initial_state))  
 
-    explored = set()
-    
+    parent = None
+    best_cost = 0
+
+    while True:
+        if not nodes:
+            return "failure"
+        
+        #remove-front (nodes)
+        state = heapq.heappop(nodes)[1] 
+        print("popped:", state)
+
+        if goal_state == state:
+            print("Found goal state!")
+            return state
+        
+        children = expand(state)
+        nodes = QUEUING_FUNCTION(nodes, state, children, parent, best_cost)
+
+    parent = state
     pass
+
+def QUEUING_FUNCTION(nodes, state, children, parent, best_cost):
+    return nodes
+
+def expand(state):
+    return []
+
 
 
 puzzle = custom_puzzle()
 print("Puzzle tuple:", puzzle)
 
-
-
-
+result = general_search(puzzle, QUEUING_FUNCTION)
+print("Result:", result)
